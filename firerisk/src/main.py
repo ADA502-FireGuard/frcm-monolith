0f435999b593a5e8c91afaecbff52126ae143b21
tmavro <576304@stud.hvl.no>
2024-05-18 16:08:37 +0200

    Added microservices as submodule repositories

--- firerisk/src/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from frcm.routes import router
-
-app = FastAPI()
-app.include_router(router)
-
-@app.get("/")
-async def root():
-    return {"message": "FireGuard Cloud Service"}
--- postgres-frcm/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Env var for non-interactive login
-export PGPASSWORD=password
-
-# Connect to the PostgreSQL database and execute the cleanup SQL query
-psql -h postgres-frcm_postgres_1 -U admin -d fireriskdb -c "DELETE FROM fireriskdata WHERE CURRENT_TIMESTAMP >= ExpirationDate;"
